672d4a1681632d6472322b9c75ae31416f01b433
AeroNotix <aaron.l.france@gmail.com>
2012-12-10 13:17:04 +0100

    Added a simply notification script to ping Qtile with.

---
+++ simple_notify.py
@@ -0,0 +1,36 @@
+import weechat
+import subprocess
+
+from libqtile.command import Client
+
+c = Client()
+
+USERNAME = "aero"
+
+def currentbuffer():
+    buf = weechat.current_buffer()
+    return "#"+weechat.buffer_get_string(buf, "short_name")
+
+weechat.register(
+    "qtile_notify", "AeroNotix", "0.1", "GPL3", "Relay Privmessages to QTile", "", ""
+    )
+
+def catchpriv(data, signal, signal_data):
+    
+    m = weechat.info_get_hashtable(
+        "irc_message_parse",
+        {"message": signal_data},
+        )
+
+    if m['channel'] != currentbuffer() and USERNAME in m['arguments']:
+        subprocess.call(
+            ["mpg123","/home/aero/.notice.mp3"],
+            stderr=open("/dev/null", "w"),
+            stdin=open("/dev/null", "w")
+            )
+    if USERNAME in m['arguments']:
+        c.widget["notice"].update(m['channel'])
+    return weechat.WEECHAT_RC_OK
+
+weechat.hook_signal("*,irc_in_PRIVMSG", "catchpriv", "")
+
